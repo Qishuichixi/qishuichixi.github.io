03d5acf69c4ad9651fdbe2182ce9d0e012310f0b
qishuichixi <qishuichixi@126.com>
2017-07-20 18:35:02 +0800

    Site updated: 2017-07-20 18:35:00

--- js/photo.js
+++ js/photo.js
@@ -18,8 +18,8 @@
                 var html, li = "";
                 for (var i = begin; i < end && i < data.length; i++) {
                     li += '<li><div class="img-box">' +
-                        '<a class="img-bg" rel="example_group" href="http://ot2rz6fmw.bkt.clouddn.com/' + data[i] + '?raw=true"></a>' +
-                        '<img lazy-src="http://ot2rz6fmw.bkt.clouddn.com/' + data[i] + '?raw=true" />' +
+                        '<a class="img-bg" rel="example_group" href="http://ot2rz6fmw.bkt.clouddn.com/' + data[i] ></a>' +
+                        '<img lazy-src="http://ot2rz6fmw.bkt.clouddn.com/' + data[i]  />' +
                         '</li>';
                 }
 
