283159edfa66cdf72bb34f629c8cc91eba3b4a3e
qishuichixi <qishuichixi@126.com>
2017-07-24 00:40:29 +0800

    Site updated: 2017-07-24 00:40:27

--- photos/ins.js
+++ photos/ins.js
@@ -129,7 +129,7 @@
                 <figcaption style="display:none" itemprop="caption description">' + data.text[i] + '</figcaption>\
             </figure>';
         }
-        ulTmpl = ulTmpl + '<section class="archives album"><h1 class="year">' + data.year + '年<em>' + data.month + '月</em><br>' + data.description + '<br><br></h1>\
+        ulTmpl = ulTmpl + '<section class="archives album"><h1 class="year">' + data.year + '年' + data.month + '月' + data.day + '<br>' + data.description + '<br><br></h1>\
         <ul class="img-box-ul">' + liTmpl + '</ul>\
         </section>';
       }
